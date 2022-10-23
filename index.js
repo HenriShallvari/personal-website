5918e6e09468e7cc44abbea862a2c1c1e0716b3b
HenriShallvari <77007119+HenriShallvari@users.noreply.github.com>
2022-10-24 00:00:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let counter = 0;
+let hero_img = document.getElementById('hero-image');
+let thug_img = document.getElementById('image');
+let body = document.getElementsByTagName('body');
+
+function showGlasses(){
+    counter++;
+
+    if(counter == 10){
+     thug_img.setAttribute('src', 'assets/davinci-glasses.png');
+     thug_img.style.width = '47vw';
+     hero_img.style.bottom  = '-31%';
+     hero_img.style.right  = '3%';
+    }
+}
